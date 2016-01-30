31f6e7d97eb7722c8d373e81402241421970b07f
EMallmann <ericmallmann@gmail.com>
2016-01-30 14:55:09 -0600

    New Autonomous Object
    Needs further work, maybe an h file.

---
+++ Autonomous.cpp
@@ -0,0 +1,12 @@
+#include "WPILib.h"
+Autonomous::Autonomous(){
+  //pass in encoders
+  //pass in motor controllers
+  //sendable chooser + options
+  
+  bool driveOverDefense(RobotDrive rd){
+    bool done= false;
+    //driveDistance(....);
+    return done;
+  }
+}
