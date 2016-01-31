a60efe4c749233ec468fe390a3b03374440fef42
EMallmann <ericmallmann@gmail.com>
2016-01-31 16:32:41 -0600

    Condensed File
    Added other classes for easy reading :)

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,69 +1,40 @@
 #include "WPILib.h"
-
+#include "DriveTrain.h"
+#include "Autonomous.h"
 class Robot: public IterativeRobot{
 private:
 	LiveWindow *lw = LiveWindow::GetInstance();
 	SendableChooser *chooser;
-	
-	Victor *victor1;
-	Victor *victor2;
-	Victor *victor3;
-	Victor *victor4;
-	
-	Joystick *arcadeStick;
-	
-	RobotDrive *myRobot;
-	
-	Timer *timer;
-	
-	Encoder *leftEncoder; 
+
+	Joystick *stick;
+	Autonomous *autonomous;
+	DriveTrain *Drive;
+
+
+
+	Encoder *leftEncoder;
 	Encoder *rightEncoder;
-	
-	double leftStart=0.0; 
+
+	double leftStart=0.0;
 	double rightStart=0.0;
 	int step=0;
 
-	const std::string autoNameDefault = "Default";
-	const std::string autoNameCustom = "My Auto";
-	std::string autoSelected;
+
 
 	void RobotInit(){
-		
-		victor1= new Victor(0);
-		victor2= new Victor(1);
-		victor3= new Victor(2);
-		victor4= new Victor(3);
-		myRobot= new RobotDrive(victor1, victor2, victor3, victor4);
-		arcadeStick= new Joystick(0);
-		chooser = new SendableChooser();
-		chooser->AddDefault(autoNameDefault, (void*)&autoNameDefault);
-		chooser->AddObject(autoNameCustom, (void*)&autoNameCustom);
-		SmartDashboard::PutData("Auto Modes", chooser);
-		
-		
-		timer= new Timer();
+
+
+		Drive= new DriveTrain();
+		stick= new Joystick(0);
+
+
 		leftEncoder = new Encoder (24,25, true, CounterBase:: k4X); //check ports
 		rightEncoder = new Encoder (16,17, true, CounterBase:: k4X);
 		leftEncoder->SetDistancePerPulse(6.0*3.14/128.0); //check circumference/(pulses per revolution)
 		rightEncoder->SetDistancePerPulse(6.0*3.14/256.0);
+		autonomous = new Autonomous(Drive);
 	}
-	void DriveSet(float speed, float angle){
-		myRobot->ArcadeDrive(speed, angle, true);
-	}
-	
-	bool driveDistance(int inches, double speed){
-		bool done = false;
-		double leftDis= leftEncoder->GetDistance()-leftStart;
-		double rightDis= rightEncoder->GetDistance()-rightStart;
-		if (leftDis<inches && leftDis>-inches && rightDis<inches && rightDis>-inches){
-			Drive->DriveSet(speed,0.0);
-		}
-		else{
-			Drive->DriveSet(0.0,0.0);
-			done=true;
-		}
-		return done;
-	}
+
 
 	/**
 	 * This autonomous (along with the chooser code above) shows how to select between different autonomous modes
@@ -75,70 +46,29 @@ private:
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
 	void AutonomousInit(){
-		autoSelected = *((std::string*)chooser->GetSelected());
-		std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
-		std::cout << "Auto selected: " << autoSelected << std::endl;
-		
+
 		leftEncoder->Reset();
 		rightEncoder->Reset();
 		rightStart=0.0;
 		leftStart=0.0;
-		
-		
-		timer->Start();
-		if(autoSelected == autoNameCustom){
-			//Custom Auto goes here
-		} else {
-			//Default Auto goes here
-		}
-	}
-	
-	void nextStep(bool done){
-		if (done){
-		  	step++;
-			time->Reset();
-			leftStart=leftEncoder->GetDistance();
-			rightStart= rightEncoder->GetDistance();
-		}
-	}
-	
-	void doNothing(){
-		Drive->DriveSet(0.0,0.0);
+
+		autonomous->startTimer();
+
 	}
 
 	void AutonomousPeriodic(){
-		if(autoSelected == autoNameCustom){
-			//Custom Auto goes here
-			if (timer->Get()<5)
-			{
-				myRobot->Drive(1.0,0.0);
-			}
-			else
-			{
-				myRobot->Drive(0.0,0.0);
-			}
-		} else {
-			//Default Auto goes here
-		}
+			autonomous->driveOverDefense();
+
 	}
 
 	void TeleopInit(){
 		rightEncoder->Reset();
 		leftEncoder->Reset();
 	}
-	double getThrottle(double val){
-		 float throttle = arcadeStick->GetThrottle();
-		 throttle++;
-		 throttle = throttle *((1-val)/2);
-		 throttle+= val;
-		 return throttle;
-	}
 
 	void TeleopPeriodic(){
-		float x= arcadeStick->GetY();
-		float y= arcadeStick->GetTwist();
-		double sensitivity= getThrottle(.5);
-		myRobot->ArcadeDrive(x*sensitivity,y*sensitivity,true);
+		Drive->Drive();
+
 	}
 
 	void TestPeriodic(){
