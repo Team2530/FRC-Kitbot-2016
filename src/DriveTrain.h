d448e3d85ee401d5ee5113fee24c5f248b82727e
EMallmann <ericmallmann@gmail.com>
2016-01-31 16:34:24 -0600

    Drive Train Header File
    Daddy for DriveTrain.cpp

--- src/DriveTrain.h
+++ src/DriveTrain.h
@@ -1,30 +1,39 @@
-#ifndef _DRIVETRAIN_H_
-#define _DRIVETRAIN_H_
+/*
+ * DriveTrain.h 
+ *
+ *  Created on: Jan 31, 2016
+ *      Authors: Eric and Jack
+ */
+#include "WPILib.h"
+#ifndef SRC_DRIVETRAIN_H_
+#define SRC_DRIVETRAIN_H_
+
 class DriveTrain {
 
 	Victor *vic1;
 	Victor *vic2;
-  Victor *vic3;
+	Victor *vic3;
 	Victor *vic4;
-	
+
 	Joystick *stick;
 
-  RobotDrive *myRobot;
-	
+	RobotDrive *myRobot;
+
 	Encoder *leftEncoder;
 	Encoder *rightEncoder;
-	
+
 public:
 	DriveTrain();
-	
 
-
-	
-	
 	double getThrottle(double);
 	void DriveSet(float, float);
-	
+
 	void AutonomousInit();
 	void TeleopInit();
-	};
-#endif
+	void Drive();
+	bool driveDistance(int,double);
+
+};
+
+#endif /* SRC_DRIVETRAIN_H_ */
+
